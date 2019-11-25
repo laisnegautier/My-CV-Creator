3dd182683617addafb1a7a9db3dc63ad88fe73e8
saudrix <saudrix@gmail.com>
2019-11-25 11:43:19 +0100

    Adding file to hold the presenters

---
+++ CV_Creator/App/Presenter/MainPresenter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using App.View;
+using DAL;
+
+namespace App.Presenter
+{
+    public class MainPresenter
+    {
+        private IResumeRepository _resumeRepository;
+        private IMainView _view;
+
+        public MainPresenter(IResumeRepository resumeRepository, IMainView view)
+        {
+            _resumeRepository = resumeRepository;
+            _view = view;
+        }
+    }
+}
