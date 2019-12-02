20a9393f65ed49320cd87710d6d42ffb00562afe
Gautier Laisne <glaisne@ensc.fr>
2019-12-02 09:56:35 +0100

    Tests unitaires mis en place

---
+++ DALTests/ResumeRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Domain;
+
+namespace DAL.Tests
+{
+    [TestClass()]
+    public class ResumeRepositoryTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            // Marche
+            IResumeRepository testResume;
+            testResume = new ResumeRepository();
+            List<Resume> Resumes = testResume.GetAll();
+
+
+            Assert.IsTrue(Resumes[0].Title == "Mon_CV1");
+        }
+    }
+}
