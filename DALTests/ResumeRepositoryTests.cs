5969431d0ac4d77ff91f790a2ae2930521e45b15
Gautier Laisne <glaisne@ensc.fr>
2019-12-02 10:16:24 +0100

    Passage aux tests unitaires au lieu de tests consoles

--- DALTests/ResumeRepositoryTests.cs
+++ DALTests/ResumeRepositoryTests.cs
@@ -24,5 +24,46 @@ namespace DAL.Tests
 
             Assert.IsTrue(Resumes[0].Title == "Mon_CV1");
         }
+
+        /* Marche
+        IResumeRepository testResume;
+        testResume = new ResumeRepository();
+        List<Resume> Resumes = testResume.GetAll();
+
+            foreach (Resume r in Resumes)
+            {
+                Console.WriteLine(r.Title);
+                Console.WriteLine(r.Containers.Count());
+
+                for(int i = 0; i<r.Containers.Count; i++)
+                    Console.WriteLine("Valeur name : " + r.Containers[i].Name);
+            }
+
+    Console.WriteLine("-----------------------------------------------");
+
+            IContainerRepository testContainer;
+    testContainer = new ContainerRepository();
+    List<Container> Containers = testContainer.GetAll();
+
+    Console.WriteLine(Containers.Count);
+            foreach (Container c in Containers)
+            {
+                Console.WriteLine(c.Name);
+            }
+
+Console.WriteLine("-----------------------------------------------");
+
+            IParagraphRepository testParagraph;
+testParagraph = new ParagraphRepository();
+List<Paragraph> Paragraphs = testParagraph.GetAll();
+
+Console.WriteLine(Paragraphs.Count);
+            foreach (Paragraph p in Paragraphs)
+            {
+                Console.WriteLine(p.Content);
+            }
+
+            Console.WriteLine("test : " + testParagraph.GetById(3));
+            */
     }
 }
