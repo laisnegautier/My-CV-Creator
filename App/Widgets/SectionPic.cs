5cb0acc5660f42dd2cb45a4e98bdc17e21bf528d
saudrix <saudrix@gmail.com>
2019-12-07 15:07:54 +0100

    Start the work on SectionPicc Widget

---
+++ App/Widgets/SectionPic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App.Widgets
+{
+    public partial class SectionPic : UserControl
+    {
+        public SectionPic()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
