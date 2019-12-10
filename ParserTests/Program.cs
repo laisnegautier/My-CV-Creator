3e3c12b1271e7e9ba171a4c38e656cf605eb0150
Gautier <laisne.gautier@gmail.com>
2019-12-10 16:45:26 +0100

    intialisation d'iText7 pour parserPDF

---
+++ ParserTests/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace ParserTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PdfWriter pdfWriter = new PdfWriter("hello.pdf"))
+            using (PdfDocument pdfDocument = new PdfDocument(pdfWriter))
+            using (Document document = new Document(pdfDocument))
+            {
+                document.Add(new Paragraph("Hello World!"));
+            }
+        }
+    }
+}
